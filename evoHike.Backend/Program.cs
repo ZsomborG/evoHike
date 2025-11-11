dceea389202c6f78d6a35e17d2397d8d2fe0dfb6
vigeomart <veresmartin2006@gmail.com>
2025-11-11 17:35:41 +0100

    megse

--- evoHike.Backend/Program.cs
+++ evoHike.Backend/Program.cs
@@ -26,11 +26,7 @@ builder.Services.AddCors(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    var xmlFilename = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
