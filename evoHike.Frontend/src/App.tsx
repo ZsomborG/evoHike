3cf1710791fb2771f5c47c89ada3e3f9a89e7dc7
Pejkó Bálint <61847355+balintpejko@users.noreply.github.com>
2025-11-18 00:29:13 +0100

    NavBar component created, basic routing added with react-router-dom

--- evoHike.Frontend/src/App.tsx
+++ evoHike.Frontend/src/App.tsx
@@ -1,41 +1,47 @@
 import './App.css';
 import type { WeatherForecast } from './types/api';
-import Button from './components/Button'
+import { useApi } from './hooks/useApi';
+import Button from './components/Button';
 import LoadingSpinner from './components/LoadingSpinner';
 import ErrorMessage from './components/ErrorMessage';
 
 function App() {
-  const { data:forecasts, loading, error, refetch } = useApi<WeatherForecast[]>('/weatherforecast', { manual: true });
+  const {
+    data: forecasts,
+    loading,
+    error,
+    refetch,
+  } = useApi<WeatherForecast[]>('/weatherforecast', { manual: true });
   return (
-    <div className="App">
-      <h1>Weather Forecast from C# Backend</h1>
-      <Button onClick={refetch}>Click Here</Button>
-      {loading && <LoadingSpinner />}
-      {!loading && error && <ErrorMessage>{error}</ErrorMessage>}
+      <div className="App">
+        <h1>Weather Forecast from C# Backend</h1>
+        <Button onClick={refetch}>Click Here</Button>
+        {loading && <LoadingSpinner />}
+        {!loading && error && <ErrorMessage>{error}</ErrorMessage>}
 
-      {!loading && Array.isArray(forecasts) && (
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Temp. (C)</th>
-              <th>Temp. (F)</th>
-              <th>Summary</th>
-            </tr>
-          </thead>
-          <tbody>
-            {forecasts.map((forecasts) => (
-              <tr key={forecasts.date}>
-                <td>{new Date(forecasts.date).toLocaleDateString()}</td>
-                <td>{forecasts.temperatureC}</td>
-                <td>{forecasts.temperatureF}</td>
-                <td>{forecasts.summary}</td>
+        {!loading && Array.isArray(forecasts) && (
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Temp. (C)</th>
+                <th>Temp. (F)</th>
+                <th>Summary</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+            </thead>
+            <tbody>
+              {forecasts.map((forecasts) => (
+                <tr key={forecasts.date}>
+                  <td>{new Date(forecasts.date).toLocaleDateString()}</td>
+                  <td>{forecasts.temperatureC}</td>
+                  <td>{forecasts.temperatureF}</td>
+                  <td>{forecasts.summary}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
   );
 }
 export default App;
