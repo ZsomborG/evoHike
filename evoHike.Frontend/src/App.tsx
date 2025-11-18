0c70ec8f5858deef2706640342f1afa069afc96e
Pejkó Bálint <61847355+balintpejko@users.noreply.github.com>
2025-11-18 11:40:51 +0100

    Program.cs fix, frontend endpoint modify

--- evoHike.Frontend/src/App.tsx
+++ evoHike.Frontend/src/App.tsx
@@ -11,37 +11,37 @@ function App() {
     loading,
     error,
     refetch,
-  } = useApi<WeatherForecast[]>('/weatherforecast', { manual: true });
+  } = useApi<WeatherForecast[]>('/api/weatherforecast', { manual: true });
   return (
-      <div className="App">
-        <h1>Weather Forecast from C# Backend</h1>
-        <Button onClick={refetch}>Click Here</Button>
-        {loading && <LoadingSpinner />}
-        {!loading && error && <ErrorMessage>{error}</ErrorMessage>}
+    <div className="App">
+      <h1>Weather Forecast from C# Backend</h1>
+      <Button onClick={refetch}>Click Here</Button>
+      {loading && <LoadingSpinner />}
+      {!loading && error && <ErrorMessage>{error}</ErrorMessage>}
 
-        {!loading && Array.isArray(forecasts) && (
-          <table>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Temp. (C)</th>
-                <th>Temp. (F)</th>
-                <th>Summary</th>
+      {!loading && Array.isArray(forecasts) && (
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Temp. (C)</th>
+              <th>Temp. (F)</th>
+              <th>Summary</th>
+            </tr>
+          </thead>
+          <tbody>
+            {forecasts.map((forecasts) => (
+              <tr key={forecasts.date}>
+                <td>{new Date(forecasts.date).toLocaleDateString()}</td>
+                <td>{forecasts.temperatureC}</td>
+                <td>{forecasts.temperatureF}</td>
+                <td>{forecasts.summary}</td>
               </tr>
-            </thead>
-            <tbody>
-              {forecasts.map((forecasts) => (
-                <tr key={forecasts.date}>
-                  <td>{new Date(forecasts.date).toLocaleDateString()}</td>
-                  <td>{forecasts.temperatureC}</td>
-                  <td>{forecasts.temperatureF}</td>
-                  <td>{forecasts.summary}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 }
 export default App;
