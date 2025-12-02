fac26f4f02a9b7fc7859e13fa21ee0dc3957c99d
Pejkó Bálint <61847355+balintpejko@users.noreply.github.com>
2025-12-03 00:43:38 +0100

    Route display tests

--- evoHike.Frontend/src/pages/RoutePage.tsx
+++ evoHike.Frontend/src/pages/RoutePage.tsx
@@ -1,12 +1,22 @@
-import '../styles/RoutPageStyles.css'
+import '../styles/RoutPageStyles.css';
+import routeData from '../data/routes.json' assert { type: 'json' };
+import type { FeatureCollection } from 'geojson';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+  GeoJSON,
+} from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
 import { divIcon, point } from 'leaflet';
 
 interface Cluster {
   getChildCount: () => number;
 }
+const geojson=routeData as FeatureCollection;
 
 // custom cluster icon
 const createClusterCustomIcon = (cluster: Cluster) => {
@@ -16,30 +26,40 @@ const createClusterCustomIcon = (cluster: Cluster) => {
     iconSize: point(33, 33, true),
   });
 };
+{/* Beégetett koordináta adatok */}
+const routeCoordinates: [number, number][] = [
+  [37.7749, -122.4194], // San Francisco
+  [36.7783, -119.4179], // California center
+  [34.0522, -118.2437], // Los Angeles
+];
 
 export default function App() {
   return (
-    <div className='route-page-wrapper'>
-        <h1 style={{textAlign:'center'}}>Útvonaltervezés</h1>
-            <MapContainer className='map' center={[48.1007, 20.7897]} zoom={13}>
-                {/* OPEN STREEN MAPS TILES */}
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <MarkerClusterGroup chunkedLoading iconCreateFunction={createClusterCustomIcon}>
-                    {/* Hard coded markers */}
-                    <Marker position={[51.505, -0.09]}>
-                    <Popup>This is popup 1</Popup>
-                    </Marker>
-                    <Marker position={[51.504, -0.1]}>
-                    <Popup>This is popup 2</Popup>
-                    </Marker>
-                    <Marker position={[51.5, -0.09]}>
-                    <Popup>This is popup 3</Popup>
-                    </Marker>
-                </MarkerClusterGroup>
-        </MapContainer>
-  </div>
+    <div className="route-page-wrapper">
+      <h1 style={{ textAlign: 'center' }}>Útvonaltervezés</h1>
+      <MapContainer className="map" center={[48.1007, 20.7897]} zoom={13}>
+        {/* OPEN STREEN MAPS TILES */}
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Polyline positions={routeCoordinates} />   {/* Beégetett koordináta adatok megjelenitése */}
+        <GeoJSON data={geojson} />   {/* Fájlból koordináta adatok beolvásasa és megjelenitése */}
+        <MarkerClusterGroup
+          chunkedLoading
+          iconCreateFunction={createClusterCustomIcon}>
+          {/* Hard coded markers */}
+          <Marker position={[51.505, -0.09]}>
+            <Popup>This is popup 1</Popup>
+          </Marker>
+          <Marker position={[51.504, -0.1]}>
+            <Popup>This is popup 2</Popup>
+          </Marker>
+          <Marker position={[51.5, -0.09]}>
+            <Popup>This is popup 3</Popup>
+          </Marker>
+        </MarkerClusterGroup>
+      </MapContainer>
+    </div>
   );
 }
