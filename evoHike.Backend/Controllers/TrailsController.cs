4ce5cf85f138ea7b44198f61cf07e7f6cd028a3e
Histvan <90707933+histvan15@users.noreply.github.com>
2026-01-07 10:16:36 +0100

    TrailsController létrehozás és program.cs tisztítása. (#44)
    * TrailsController létrehozás és program.cs tisztítása.
    
    * Service réteg hozzáadása és TrailController-ből logika kivétele.
    
    * Hibakezelés kiegészítés és felesleges kód eltávolítása

---
+++ evoHike.Backend/Controllers/TrailsController.cs
@@ -0,0 +1,25 @@
+﻿using evoHike.Backend.Models;
+using evoHike.Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace evoHike.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TrailsController(ITrailService _trailService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Trail>>> GetTrails()
+        {
+            try
+            {
+                var trails = await _trailService.GetAllTrailsAsync();
+                return Ok(trails);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
